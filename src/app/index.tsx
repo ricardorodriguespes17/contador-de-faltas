0a170092d534f99bbbd02678f810c9acf6561b4c
Ricardo Rodrigues <ricardor662@gmail.com>
2024-03-03 20:01:52 -0300

    initial commit

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { StatusBar, View } from "react-native"
+
+const Home = () => {
+    return (
+        <View className="flex flex-col">
+            <StatusBar barStyle="light-content" backgroundColor="#0d9488" />
+        </View>
+    )
+}
+
+export default Home
