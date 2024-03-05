ff68cd2f9e84058d52e95773d81ddc9e44f39747
Ricardo Rodrigues <ricardor662@gmail.com>
2024-03-05 01:47:54 -0300

    fix bug: text input color

--- src/components/InputField/index.tsx
+++ src/components/InputField/index.tsx
@@ -11,7 +11,7 @@ const InputField = ({ label, value, onChangeText }: InputFieldProps) => {
         <View className="flex my-4 w-full">
             <Text className="text-xl dark:text-white">{label}</Text>
             <TextInput
-                className="w-full h-10 border-2 border-gray-300 pl-3 rounded-md dark: text-white dark:border-gray-700"
+                className="w-full h-10 border-2 border-gray-300 pl-3 rounded-md dark:text-white dark:border-gray-700"
                 onChangeText={onChangeText}
                 value={value}
             />
